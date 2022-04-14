8150f188e177c32b06e2cf7cdfc46af6e903272c
Erik Arakelyan <erikarakelyan1997@gmail.com>
2022-04-14 15:41:53 +0400

    double quotes to single

--- spacy_loggers/aim_logger.py
+++ spacy_loggers/aim_logger.py
@@ -12,7 +12,7 @@ try:
     from aim import Run, Repo,Image,Text  # noqa: F401
 except ImportError:
     raise ImportError(
-        "We cannot find the 'aim' package on your system. Please make sure to install it."
+        'We cannot find the "aim" package on your system. Please make sure to install it.'
     )
 
 
@@ -20,7 +20,7 @@ except ImportError:
 
 # entry point: spacy.AimLogger.v1
 def aim_logger_v1(
-    repo: Optional[str] = ".",
+    repo: Optional[str] = '.',
     experiment_name: Optional[str] = None,
     run_hash: Optional[str] = None,
 
@@ -28,12 +28,12 @@ def aim_logger_v1(
     
     console = console_logger(progress_bar=False)
     # aim_run, console_log_step, console_finalize = setup_aim(experiment_name=experiment_name)
-    def setup_aim(nlp: "Language", stdout: IO = sys.stdout, stderr: IO = sys.stderr, experiment_name:str="") \
+    def setup_aim(nlp: 'Language', stdout: IO = sys.stdout, stderr: IO = sys.stderr, experiment_name:str='') \
         -> Union[Run, Callable[[Dict[str, Any]], None], Callable[[], None]]:
             config = nlp.config.interpolate()
 
             aim_run = Run(repo = repo, experiment=experiment_name, run_hash=run_hash)
-            aim_run["config"] = config
+            aim_run['config'] = config
 
             console = console_logger(progress_bar=False)
             console_log_step, console_finalize = console(nlp, stdout, stderr)
@@ -41,21 +41,21 @@ def aim_logger_v1(
             def aim_log_step(info: Optional[Dict[str, Any]]):
                 console_log_step(info)
                 if info is not None:
-                    epoch = info["epoch"]
+                    epoch = info['epoch']
                     step = None
 
-                    score = info["score"]
-                    other_scores = info["other_scores"]
-                    losses = info["losses"]
-                    aim_run.track(score , name = "Score", context = {"type":"score"})
+                    score = info['score']
+                    other_scores = info['other_scores']
+                    losses = info['losses']
+                    aim_run.track(score , name = 'Score', context = {'type':'score'})
                     if losses:
                         for loss_name, loss_value in losses.items():
-                            aim_run.track(loss_value, name=loss_name, context={"type":f"loss_{loss_name}"}, epoch=epoch, step=step)
+                            aim_run.track(loss_value, name=loss_name, context={'type':f'loss_{loss_name}'}, epoch=epoch, step=step)
 
                     if isinstance(other_scores, dict):
                         for score_name, loss_value in other_scores.items():
                             if not isinstance(loss_value, dict):
-                                aim_run.track(loss_value, name=loss_name, context={"type":f"other_scores_{score_name}"}, epoch=epoch, step=step)
+                                aim_run.track(loss_value, name=loss_name, context={'type':f'other_scores_{score_name}'}, epoch=epoch, step=step)
 
 
             def aim_finalize():
